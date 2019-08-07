658e625a4aa3a487ab36ef894919f3d719db4a2b
Hex-Zero <hex0@live.com>
2019-08-07 13:59:51 +0100

    Made the slide work

--- src/components/VideoCards.js
+++ src/components/VideoCards.js
@@ -1,15 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import blade from "../assets/topcarousel/blade.gif"
 import slide1 from "../assets/slide.png"
 import { FaGithub, FaGlobe } from "react-icons/fa"
-
 import styled, { keyframes } from "styled-components"
+
 const VideoCards = () => {
+  const [state, setstate] = useState()
+
+  useEffect(() => {}, [])
+
+  const handleSlideOut = () => {
+    setstate(slideout)
+  }
+  const handleSlideIn = () => {
+    setstate(slidein)
+  }
   return (
     <Stlyed>
       <div className="card">
-        <img className="video" src={blade} alt="one" />
-        <div className="slide">
+        <img onMouseEnter={handleSlideIn} className="video" src={blade} alt="one" />
+        <Slide onMouseLeave={handleSlideOut} animation={state} className="slide">
           <h1 className="title">Blade and Soul</h1>
           <p className="content">Action packed anime series</p>
           <div className="links">
@@ -20,19 +30,40 @@ const VideoCards = () => {
               <FaGlobe />
             </a>
           </div>
-        </div>
+        </Slide>
       </div>
     </Stlyed>
   )
 }
-const slide = keyframes`
-from {
+const slidein = keyframes`
+  0%{
     margin-left: 500px;
   }
-  to {
+  100% {
     margin-left: 0px;
   }
   
+`
+const slideout = keyframes`
+  0%{
+    margin-left: 0px;
+  }
+  100% {
+    margin-left: 500px;
+  }
+  
+`
+const Slide = styled.div`
+  color: White;
+  margin-left: 500px;
+  text-shadow: 2px 2px red;
+  background-image: url(${slide1});
+  background-size: 500px 280px;
+  border-radius: 5px;
+  width: 500px;
+  position: relative;
+  height: 280px;
+  animation: ${props => props.animation} 2s ease forwards;
 `
 
 const Stlyed = styled.div`
@@ -46,18 +77,6 @@ const Stlyed = styled.div`
     overflow: hidden;
     width: 500px;
     border-radius: 5px;
-  }
-  .slide {
-    color: White;
-    text-shadow: 2px 2px red;
-    margin: 0;
-    background-image: url(${slide1});
-    background-size: 500px 280px;
-    border-radius: 5px;
-    width: 500px;
-    position: relative;
-    animation: ${slide} 4s infinite;
-    height: 280px;
   }
   .video {
     position: absolute;
