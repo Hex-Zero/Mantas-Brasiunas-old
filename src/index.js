f3dca28ad45f6dab1714f3689258d0493ba69536
Hex-Zero <hex0@live.com>
2019-08-18 23:51:47 +0100

    snap go bye bye

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import './index.css'
 import App from './App'
-import { hydrate, render } from 'react-dom'
+import ReactDOM from 'react-dom'
 
-const rootElement = document.getElementById('root')
-if (rootElement.hasChildNodes()) {
-	hydrate(<App />, rootElement)
-} else {
-	render(<App />, rootElement)
-}
+ReactDOM.render(<App />, document.getElementById('root'))
