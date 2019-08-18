f0177a50134ca2b4eb8a6e98413ca51bd3a4108d
Hex-Zero <hex0@live.com>
2019-08-18 20:36:41 +0100

    replace gifs with jpg

--- src/components/VideoCards.js
+++ src/components/VideoCards.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import Loading from '../Loading'
 import { FaGithub, FaGlobe } from 'react-icons/fa'
 import ravenouse from '../assets/ravenouse.jpg'
-import first from '../assets/first.gif'
-import vita from '../assets/vita.gif'
+import first from '../assets/first.jpg'
+import vita from '../assets/vita.jpg'
 import budget from '../assets/budget.jpg'
 const Image = React.lazy(() => import('./Image'))
 const VideoCards = ({ url, title, content, git, web }) => {
