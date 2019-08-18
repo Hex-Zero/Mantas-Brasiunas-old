14cff3b4ea7ac2c1d9429ce86274153c344f7f1e
Hex-Zero <hex0@live.com>
2019-08-18 23:43:36 +0100

    rearanging scss

--- src/components/VideoCards.js
+++ src/components/VideoCards.js
@@ -17,32 +17,23 @@ const VideoCards = React.memo(({ url, title, content, git, web }) => {
 		<div className={`videoCards`}>
 			<div
 				className='card'
-				onMouseEnter={() => handleSlide('slidein')}
-				onMouseLeave={() => handleSlide('slideout')}>
-				<React.Suspense fallback={<Loading />}>
+				onMouseEnter={() => handleSlide('slide-in')}
+				onMouseLeave={() => handleSlide('slide-out')}>
+				<React.Suspense fallback={null}>
 					<Image src={imageUrl[url]} />
 				</React.Suspense>
-				<div className={slide + ' slide '}>
-					<div className='projectSlides'>
-						<h1 className='title'>{title}</h1>
-						<h2 className='content'>{content}</h2>
-						<div className='bottom-Line'>
-							<div className='tagWrapper'>
-								<a
-									className='web tags'
-									href={web}
-									target='__blank'
-									alt='website link'>
-									<FaGlobe />
-								</a>
-								<a
-									className='git tags'
-									href={git}
-									target='__blank'
-									alt='github link'>
-									<FaGithub />
-								</a>
-							</div>
+				<Loading />
+				<div className={`${slide} projectSlides`}>
+					<h1 className='title'>{title}</h1>
+					<h2 className='content'>{content}</h2>
+					<div className='bottom-Line'>
+						<div className='tagWrapper'>
+							<a className='web tags' href={web} target='__blank' alt='website link'>
+								<FaGlobe />
+							</a>
+							<a className='git tags' href={git} target='__blank' alt='github link'>
+								<FaGithub />
+							</a>
 						</div>
 					</div>
 				</div>
