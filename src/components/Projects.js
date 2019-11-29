335661e1a3ad7e097607697e62e7994f97279863
Hex-Zero <hex0@live.com>
2019-11-29 14:22:55 +0000

    upgrade

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,59 +1,59 @@
-import React from 'react'
-import VideoCards from './VideoCards'
-import About from '../assets/About'
-import uuid from 'uuid'
+import React from "react";
+import VideoCards from "./VideoCards";
+import About from "../assets/About";
+import uuid from "uuid";
 const Projects = () => {
-	const [ cardInfo ] = React.useState([
-		{
-			url: 0,
-			title: 'Vita Smile Photograpy',
-			content: 'Landing page for a local family run photograpy business',
-			git: 'https://github.com/Hex-Zero/VitaSmilePhotography',
-			web: 'https://vita-smile-photography.uk/'
-		},
-		{
-			url: 1,
-			title: 'Ravenouse',
-			content: 'Practise reciving JSON data from Yelp API',
-			git: 'https://github.com/Hex-Zero/React-Codecademy',
-			web: 'https://hex-zero.github.io/React-Codecademy/'
-		},
-		{
-			url: 2,
-			title: 'Budget Calculator',
-			content: 'Exploration of React-Hooks in a budget calculator application',
-			git: 'https://github.com/Hex-Zero/React-Hook-App',
-			web: 'https://hex-zero.github.io/React-Hook-App/'
-		},
-		{
-			url: 3,
-			title: 'My First Website',
-			content: 'My first steps in JavaScript, Made a classic memory game',
-			git: 'https://github.com/Hex-Zero/xFirst',
-			web: 'https://hex-zero.github.io/xFirst/main.html'
-		}
-	])
+  const [cardInfo] = React.useState([
+    {
+      url: 0,
+      title: "Makeup Islang",
+      content: "Makeup Store. Best Products For Affordable Prices. ",
+      git: "https://github.com/Hex-Zero/Makeup-Island",
+      web: "https://makeupisland.netlify.com"
+    },
+    {
+      url: 1,
+      title: "Vita Smile Photograpy",
+      content: "Landing page for a local family run photograpy business",
+      git: "https://github.com/Hex-Zero/VitaSmilePhotography",
+      web: "https://vita-smile-photography.uk/"
+    },
+    {
+      url: 2,
+      title: "Ravenouse",
+      content: "Practise reciving JSON data from Yelp API",
+      git: "https://github.com/Hex-Zero/React-Codecademy",
+      web: "https://hex-zero.github.io/React-Codecademy/"
+    },
+    {
+      url: 3,
+      title: "My First Website",
+      content: "My first steps in JavaScript, Made a classic memory game",
+      git: "https://github.com/Hex-Zero/xFirst",
+      web: "https://hex-zero.github.io/xFirst/main.html"
+    }
+  ]);
 
-	return (
-		<div className='projects'>
-			<h1 className='projectTitle funGlow'>
-				My Projects <p>click a project below for more info</p>
-			</h1>
-			<div className='fbox'>
-				{cardInfo.map(({ classDummy, url, title, content, git, web }) => (
-					<VideoCards
-						classDummy={classDummy}
-						key={uuid()}
-						url={url}
-						title={title}
-						content={content}
-						git={git}
-						web={web}
-					/>
-				))}
-				<About />
-			</div>
-		</div>
-	)
-}
-export default Projects
+  return (
+    <div className="projects">
+      <h1 className="projectTitle ">
+        My Projects <p>click a project below for more info</p>
+      </h1>
+      <div className="fbox">
+        {cardInfo.map(({ classDummy, url, title, content, git, web }) => (
+          <VideoCards
+            classDummy={classDummy}
+            key={uuid()}
+            url={url}
+            title={title}
+            content={content}
+            git={git}
+            web={web}
+          />
+        ))}
+        <About />
+      </div>
+    </div>
+  );
+};
+export default Projects;
--- src/components/VideoCards.js
+++ src/components/VideoCards.js
@@ -1,39 +1,50 @@
-import React, { useState } from 'react'
-import Loading from '../Loading'
-import { FaGithub, FaGlobe } from 'react-icons/fa'
-import ravenouse from '../assets/ravenouse.jpg'
-import first from '../assets/first.gif'
-import vita from '../assets/vita.gif'
-import budget from '../assets/budget.jpg'
+import React, { useState } from "react";
+import Loading from "../Loading";
+import { FaGithub, FaGlobe } from "react-icons/fa";
+import ravenouse from "../assets/ravenouse.jpg";
+import first from "../assets/first.gif";
+import vita from "../assets/vita.gif";
+import makeup from "../assets/makeup.jpg";
 const VideoCards = React.memo(({ url, title, content, git, web }) => {
-	const [ imageUrl ] = useState([ vita, ravenouse, budget, first ])
-	const [ slide, setSlide ] = useState('')
-	return (
-		<div className={`videoCards`}>
-			<div
-				className='card'
-				onMouseEnter={() => setSlide('slide-in')}
-				onMouseLeave={() => setSlide('slide-out')}>
-				<img className='projectImage' src={imageUrl[url]} alt={title} />
-				<Loading />
-				<div className={`${slide} projectSlides`}>
-					<h1 className='title'>{title}</h1>
-					<h2 className='content'>{content}</h2>
-					<div className='bottom-Line'>
-						<div className='tagWrapper'>
-							<a className='web tags' href={web} target='__blank' alt='website link'>
-								<FaGlobe />
-								<p> Site</p>
-							</a>
-							<a className='git tags' href={git} target='__blank' alt='github link'>
-								<FaGithub />
-								<p> Code</p>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-})
-export default VideoCards
+  const [imageUrl] = useState([makeup, vita, ravenouse, first]);
+  const [slide, setSlide] = useState("");
+  return (
+    <div className={`videoCards`}>
+      <div
+        className="card"
+        onMouseEnter={() => setSlide("slide-in")}
+        onMouseLeave={() => setSlide("slide-out")}
+      >
+        <img className="projectImage" src={imageUrl[url]} alt={title} />
+        <Loading />
+        <div className={`${slide} projectSlides`}>
+          <h1 className="title">{title}</h1>
+          <h2 className="content">{content}</h2>
+          <div className="bottom-Line">
+            <div className="tagWrapper">
+              <a
+                className="web tags"
+                href={web}
+                target="__blank"
+                alt="website link"
+              >
+                <FaGlobe />
+                <p> Site</p>
+              </a>
+              <a
+                className="git tags"
+                href={git}
+                target="__blank"
+                alt="github link"
+              >
+                <FaGithub />
+                <p> Code</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
+export default VideoCards;
